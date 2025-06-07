90bfb71c4914a475c9b3f79d77790715e6affb10
Ryan Frazin <RyanFrazin@Gmail.com>
2025-06-07 00:45:03 -0700

    add animation and audio to buttons

--- game.js
+++ game.js
@@ -2,10 +2,11 @@ gamePattern = [];
 buttonColors = ["red", "blue", "green", "yellow"];
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4); // Generates a random number between 1 and 3
-
     var randomChosenColor = buttonColors[randomNumber];
-
     gamePattern.push(randomChosenColor);
 
-    console.log(gamePattern);
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+    const audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
 }
